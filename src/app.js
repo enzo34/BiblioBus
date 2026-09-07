20995ed79bca7da154df8fe3c0d5717d9cecf85e
Enzo <eclavelloux@gmail.com>
2026-09-07 12:32:26 +0200

    core: setup connection to database in app

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+    res.json({status: 'ok', service: 'bibilobus-api'});
+});
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const app = require('./app');
+const connectDb = require('./config/db');
+
+const PORT = process.env.PORT || 3000;
+
+connectDb().then(() => {
+    app.listen(PORT, () => {
+        console.log("serveur démarré sur http://localhost:" + PORT);
+    });
+});
